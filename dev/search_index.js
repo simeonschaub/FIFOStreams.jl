f00ef123efbb48a72cee1c1bf2581d82d9d2b6bb
Documenter.jl <documenter@juliadocs.github.io>
2020-10-19 15:30:43 +0000

    build based on 4c1ed88

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FIFOStreams","category":"page"},{"location":"#FIFOStreams","page":"Home","title":"FIFOStreams","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FIFOStreams]","category":"page"}]
+}
